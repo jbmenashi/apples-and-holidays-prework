b7fda133aaa22326d3eac497c3b19fa9759ef19d
Jacob Menashi <jbmenashi@gmail.com>
2018-10-04 17:09:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   new_array = []
   holiday_hash.each do |season, holiday|
-    if season.values.include?("BBQ")
+    if season[holiday].include?("BBQ")
       new_array << season
     end
   end
