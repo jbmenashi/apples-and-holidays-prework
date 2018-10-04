9ab951c2ddf63f9736312b5e74cd9cfa43422702
Jacob Menashi <jbmenashi@gmail.com>
2018-10-04 15:55:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,11 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+    season.to_s.capitalize!
+    holiday.to_s.capitalize!
+  end
+  puts holiday_hash
 end
 
 def all_holidays_with_bbq(holiday_hash)
